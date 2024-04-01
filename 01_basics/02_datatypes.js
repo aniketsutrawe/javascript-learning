3103a1081e771694011fb629b6abf6344d69eff7
aniketsutrawe <aniketsutrawe28@gmail.com>
2024-04-01 13:17:24 +0530

    learnt datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"    //treat all JS code as newer version
+
+// alert(3 + 3)     //we are using nodejs not browser so this will won't work
+
+console.log( "3 + 3 = " + [3 + 3]);
+
+let name = "aniket" //String datatype 
+let age = 23        //Number range=> 2 to power 53
+let state;          //undefined
+let what = null //empty
+
+//boolean => true/false
+//null => standalone value
+// symbol => uniques
+
+//object
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined 
