0188e84e393161d8412ef3efdc260ea2d239476c
aniketsutrawe <aniketsutrawe28@gmail.com>
2024-04-01 17:16:51 +0530

    learnt operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
 let score = '33abc'
 
-console.log("type of score variable is " + typeof score);
+// console.log("type of score variable is " + typeof score);
 
 let valueInNumber = Number(score)
-console.log("Type of valueInNumber wich is score variable is converted in number is "+ typeof valueInNumber);
+// console.log("Type of valueInNumber wich is score variable is converted in number is "+ typeof valueInNumber);
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
@@ -13,7 +13,7 @@ console.log(valueInNumber);
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 =>false
 // "" => false
@@ -21,4 +21,25 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
+// console.log(stringNumber);
+
+
+// ****************************************** Operations ***************************************************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);     2 added by 2
+// console.log(2-2);     2 subtracted by 2
+// console.log(2*2);     2 multiplied by 3
+// console.log(2**2);    2 is to power 2
+// console.log(2**3);    2 is to power 3
+// console.log(2/3);     2 divided by 3
+// console.log(2%3);     remender of 2/3
+
+let str1 = "hello"
+let str2 = " Aniket"
+let str3 = str1 + str2
+// console.log(str3);  
