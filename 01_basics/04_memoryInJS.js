2b12e413def946d19170add493d87e04d8c93d1f
aniketsutrawe <aniketsutrawe28@gmail.com>
2024-04-02 17:00:15 +0530

    memory in JS

---
+++ 01_basics/04_memoryInJS.js
@@ -0,0 +1,22 @@
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non Primitive)
+
+// Stack : String, Number, Boolean, null, undefined, Symbol, bigInt
+
+let stringIs = "This is an String."
+
+let numberIs = 23; //this is number
+
+// Heap : Array, Objects, Functions
+
+
+let objectIs = {
+    name : "Aniket",
+    age : 23
+    //this is object
+}
+
+console.log(objectIs.name); //access the object's values using '.' like objectIs.name
