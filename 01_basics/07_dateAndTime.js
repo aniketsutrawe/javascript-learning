8b39ab46b3bceca5c9157f2d93c334a9a027c4e5
aniketsutrawe <aniketsutrawe28@gmail.com>
2024-04-04 12:56:46 +0530

    in dates time stamp

--- 01_basics/07_dateAndTime.js
+++ 01_basics/07_dateAndTime.js
@@ -11,3 +11,9 @@ let myDate = new Date()
 // let myCreatedDate = new Date("2023-01-14")
 let myCreatedDate = new Date("01-14-2023")
 // console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
