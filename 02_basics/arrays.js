fb8f4ce903ee7c3f063cee488c1137b671144eae
aniketsutrawe <aniketsutrawe28@gmail.com>
2024-04-05 11:28:14 +0530

    arrays

---
+++ 02_basics/arrays.js
@@ -0,0 +1,17 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Arrays
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// ()  =   Paranthisis
+
+// []  =    Brakets [Squre Brakets]
+
+// {}  =    Braces {Curly Braces}
+
+// Array is defined in only [Squre Brakets]
+
+let myArr = [ 0, 1, 2, 3, 4, 5, "These are elements, They can be string/numbers or anything like boolean also" ];   //Array are Resizable we can add data and remove anytime
+
+console.log(myArr[3])
