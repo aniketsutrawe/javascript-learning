598162d460469187288e8534f8050fe349febf2b
aniketsutrawe <aniketsutrawe28@gmail.com>
2024-04-05 11:41:30 +0530

    flat methods in array and array.of

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -10,3 +10,22 @@ const dc_heros = ["superman", "flash", "batman"]
 // console.log(allHeros);
 
 const all_new_heros = [...marvel_heros, ...dc_heros] //sprade operator = a glass we are holding if the glass falls it sprades on the floor sprade operator just dose like that it sprades every element
+
+console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+
+
+// console.log(Array.isArray("Hitesh"))
+// console.log(Array.from("Hitesh"))
+// console.log(Array.from({name: "hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
