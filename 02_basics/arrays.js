7e5ce8eb943f504b645aff7a05ef7c006bbcb0d5
aniketsutrawe <aniketsutrawe28@gmail.com>
2024-04-05 11:33:51 +0530

    array index

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -15,3 +15,25 @@
 let myArr = [ 0, 1, 2, 3, 4, 5, "These are elements, They can be string/numbers or anything like boolean also" ];   //Array are Resizable we can add data and remove anytime
 
 console.log(myArr[3])
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+
