4544c167a30d4b9392cb6d5b9b8bb8034645d33f
aniketsutrawe <aniketsutrawe28@gmail.com>
2024-04-05 11:40:31 +0530

    array and some of its methods

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,12 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros] //sprade operator = a glass we are holding if the glass falls it sprades on the floor sprade operator just dose like that it sprades every element
