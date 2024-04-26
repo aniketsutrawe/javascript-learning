509fa3ede5ed3bb9f8c5b77191a1efb57dbf8ef8
Aniket Sutrawe <aniketsutrawe28@gmail.com>
2024-04-26 15:45:08 +0530

    minor changes

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -11,7 +11,7 @@ const dc_heros = ["superman", "flash", "batman"]
 
 const all_new_heros = [...marvel_heros, ...dc_heros] //sprade operator = a glass we are holding if the glass falls it sprades on the floor sprade operator just dose like that it sprades every element from an array.
 
-console.log(all_new_heros);
+// console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
@@ -20,12 +20,13 @@ const real_another_array = another_array.flat(Infinity)
 
 
 
-// console.log(Array.isArray("Hitesh"))
-// console.log(Array.from("Hitesh"))
-// console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray(["Hitesh"])) //true
+console.log(Array.isArray("Hitesh")) //false
+console.log(Array.from("Anishra"))
+console.log(Array.from({name: "hitesh"})) // interesting
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+// console.log(Array.of(score1, score2, score3));
