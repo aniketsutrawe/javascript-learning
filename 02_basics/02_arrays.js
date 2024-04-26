885f07635c4eb82fa7397c4be24496124dbe441a
Aniket Sutrawe <aniketsutrawe28@gmail.com>
2024-04-26 15:40:48 +0530

    fixes

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -9,7 +9,7 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros] //sprade operator = a glass we are holding if the glass falls it sprades on the floor sprade operator just dose like that it sprades every element
+const all_new_heros = [...marvel_heros, ...dc_heros] //sprade operator = a glass we are holding if the glass falls it sprades on the floor sprade operator just dose like that it sprades every element from an array.
 
 console.log(all_new_heros);
 
